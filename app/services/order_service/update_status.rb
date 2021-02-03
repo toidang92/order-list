ee48742159de923e07b6ab43e96b910b1a991043
ToiDB <toidang92@gmail.com>
2021-02-04 00:10:55 +0700

    Fix code

--- app/services/order_service/update_status.rb
+++ app/services/order_service/update_status.rb
@@ -1,9 +1,15 @@
 module OrderService
   class UpdateStatus < BaseService
     def call(order:, status:)
+      if (status.blank?)
+        return error({
+          errors: "Select a status!"
+        })
+      end
+
       if (order.status == status)
         return error({
-          errors: "The order's status already is #{order.status_name}"
+          errors: "The order's status already is #{order.status_name}!"
         })
       end
 
